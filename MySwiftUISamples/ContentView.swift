6add6fa66725008fc94e7e4b03bf5ca2fe60c89d
TakumiOda <xiaotiantakumi@gmail.com>
2022-02-14 17:39:58 +0900

    Initial Commit

---
+++ MySwiftUISamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUISamples
+//
+//  Created by 小田匠 on 2022/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySwiftUISamples/MySwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUISamplesApp.swift
+//  MySwiftUISamples
+//
+//  Created by 小田匠 on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
