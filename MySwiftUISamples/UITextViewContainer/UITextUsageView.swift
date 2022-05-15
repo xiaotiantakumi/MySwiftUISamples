25f925ddd70faa5a57de2e9ac68d7a4f810ca136
TakumiOda <xiaotiantakumi@gmail.com>
2022-05-16 07:56:18 +0900

    カーソルをViewから呼び出して操作できるようにする修正

--- MySwiftUISamples/UITextViewContainer/UITextUsageView.swift
+++ MySwiftUISamples/UITextViewContainer/UITextUsageView.swift
@@ -7,17 +7,24 @@
 //
 
 import SwiftUI
+import Combine
 
 struct UITextUsageView: View {
     @State var text = ""
     @State var cursorPosition = 0
-    private var uiTextViewContainer: UITextViewContainer {
-        UITextViewContainer(text: $text, curPosition: $cursorPosition)
-    }
+    private var didChange = PassthroughSubject<CursorChangeableUIViewContext, Never>()
+
     var body: some View {
         VStack{
-            uiTextViewContainer.frame(height: 300).font(.title).border(.white, width: 1)
+            UITextViewContainer(didChange: didChange,text: $text, curPosition: $cursorPosition)
+                    .frame(height: 300)
+                    .font(.title).border(.white, width: 1)
             Spacer()
+            Button(action:{
+                self.didChange.send(CursorChangeableUIViewContext(type:.changeCursorPosition,direction: .left, offset: 1))
+            }){
+                Text("left")
+            }
         }
         Text(cursorPosition.description)
 
--- MySwiftUISamples/UITextViewContainer/UITextViewContainer.swift
+++ MySwiftUISamples/UITextViewContainer/UITextViewContainer.swift
@@ -7,28 +7,68 @@
 //
 
 import SwiftUI
+import Combine
 
-protocol CursorPositionSettable { // use protocol to hide implementation
-    func changeCursorPosition()
+
+struct CursorChangeableUIViewContext{
+    var type:UITextViewContainer.ApplyTypeEnum
+    var direction:UITextLayoutDirection
+    var offset:Int
 }
+
+class CursorChangeableUITextView: UITextView {
+    func changeCursorPosition(direction:UITextLayoutDirection,offset:Int){
+        // only if there is a currently selected range
+        if let selectedRange = selectedTextRange {
+            // and only if the new position is valid
+            if let newPosition = position(from: selectedRange.start, in:direction, offset: offset) {
+                // set the new position
+                selectedTextRange = textRange(from: newPosition, to: newPosition)
+            }
+        }
+    }
+}
+
 struct UITextViewContainer: UIViewRepresentable {
+    var didChange: PassthroughSubject<CursorChangeableUIViewContext, Never>
+    @State var cancellable: AnyCancellable? = nil
     @Binding var text : String
     @Binding var curPosition : Int
+    enum ApplyTypeEnum {
+        case changeCursorPosition
+    }
 
     func makeCoordinator() -> UITextViewContainer.Coordinator {
         Coordinator(self)
     }
 
-    func makeUIView(context: UIViewRepresentableContext<UITextViewContainer>) -> UITextView {
-        let inneruiTextView = UITextView(frame: .zero)
+    func makeUIView(context: UIViewRepresentableContext<UITextViewContainer>) -> CursorChangeableUITextView {
+        let inneruiTextView = CursorChangeableUITextView(frame: .zero)
         inneruiTextView.font = UIFont.systemFont(ofSize: 22)
         inneruiTextView.text = text
         inneruiTextView.delegate = context.coordinator
         context.coordinator.setup(inneruiTextView)
+
+        // doing it in `main` thread is required to avoid the state being modified during
+        // a view update
+        DispatchQueue.main.async {
+            // very important to capture it as a variable, otherwise it'll be short lived.
+            self.cancellable = didChange.sink { (value) in
+                print("Received: \(value)")
+
+                // here you can do a switch case to know which method to call
+                // on your UIKit class, example:
+                if (value.type == ApplyTypeEnum.changeCursorPosition) {
+                    // call your function!
+                    inneruiTextView.changeCursorPosition(direction: value.direction, offset: value.offset)
+                }
+            }
+        }
+
         return inneruiTextView
     }
 
-    func updateUIView(_ uiView: UITextView, context: UIViewRepresentableContext<UITextViewContainer>) {
+    func updateUIView(_ uiView: CursorChangeableUITextView, context: UIViewRepresentableContext<UITextViewContainer>) {
         uiView.text = self.text
         print("updateUIView")
     }
@@ -41,45 +81,21 @@ struct UITextViewContainer: UIViewRepresentable {
         init(_ uiTextViewContainer: UITextViewContainer) {
             self.parent = uiTextViewContainer
         }
-        var uiTextView:UITextView?
-        func setup(_ textField:UITextView) {
+        var uiTextView:CursorChangeableUITextView?
+        func setup(_ textField:CursorChangeableUITextView) {
             uiTextView = textField
             //textField.addTarget(self, action: #selector(textViewDidChange), for: .change)
         }
-
-        func changeCursorPosition(_ uiTextView: UITextView,direction:UITextLayoutDirection,offset:Int){
-            // only if there is a currently selected range
-            if let selectedRange = uiTextView.selectedTextRange {
-                // and only if the new position is valid
-                if let newPosition = uiTextView.position(from: selectedRange.start, in:direction, offset: offset) {
-                    // set the new position
-                    uiTextView.selectedTextRange = uiTextView.textRange(from: newPosition, to: newPosition)
-                }
-            }
-        }
-        func changeCursorPosition2(direction:UITextLayoutDirection,offset:Int){
-            // only if there is a currently selected range
-            guard let uiTextView = uiTextView else {return}
-            if let selectedRange = uiTextView.selectedTextRange {
-                // and only if the new position is valid
-                if let newPosition = uiTextView.position(from: selectedRange.start, in:direction, offset: offset) {
-                    // set the new position
-                    uiTextView.selectedTextRange = uiTextView.textRange(from: newPosition, to: newPosition)
-                }
-            }
-        }
-        @objc func textViewDidChange(_ uiTextView: UITextView) {
+        @objc func textViewDidChange(_ uiTextView: CursorChangeableUITextView) {
             self.parent.text = uiTextView.text ?? ""
         }
-
-
-        @objc func textViewDidChangeSelection(_ uiTextView: UITextView) {
+        @objc func textViewDidChangeSelection(_ uiTextView: CursorChangeableUITextView) {
             guard let selectedTextRange = uiTextView.selectedTextRange else {
                 return
             }
             // selectedTextRange.startはUITextPosition型なのでそこからIntにするにはこうする
             let cursorPosition = uiTextView.offset(from: uiTextView.beginningOfDocument, to: selectedTextRange.start)
-            print(cursorPosition)
+            //print(cursorPosition)
             self.parent.curPosition = cursorPosition
         }
     }
